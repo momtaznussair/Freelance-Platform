762f4f9ad7345663b210853913d2408b082cf766
Ibrahim Ali <hemamessi47@gmail.com>
2021-06-27 00:40:55 +0200

    added first and second section in landing page

--- Front-end/freelance-marketplace/src/app/components/landing-page/landing-page.module.ts
+++ Front-end/freelance-marketplace/src/app/components/landing-page/landing-page.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { JoinWorldComponent } from './join-world/join-world.component';
+import { ForClientComponent } from './for-client/for-client.component';
 
 const routes: Routes = [
 
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JoinWorldComponent,
+    ForClientComponent
+  ],
   imports: [
     CommonModule,RouterModule.forChild(routes)
-  ]
+  ],
+  exports: [JoinWorldComponent , ForClientComponent]
 })
 export class LandingPageModule { }
