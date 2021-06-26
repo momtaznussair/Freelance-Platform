d040efa5f789eecaecea66d46a6dc9dd01dc6292
Ibrahim Ali <hemamessi47@gmail.com>
2021-06-26 14:45:20 +0200

    add modules and subrouting

--- Front-end/freelance-marketplace/src/app/app-routing.module.ts
+++ Front-end/freelance-marketplace/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // example for how to use sub routes by lazy load syntax
+  {
+    path: 'landing-page',
+    loadChildren: () => import('./components/landing-page/landing-page.module').then(m => m.LandingPageModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/freelance-marketplace/src/app/app.module.ts
+++ Front-end/freelance-marketplace/src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AccordionModule} from 'primeng/accordion';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
