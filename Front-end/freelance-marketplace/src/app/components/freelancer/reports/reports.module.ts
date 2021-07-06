df3bbeec3f028c4ff178e68d6a0cc81c7ffbe5a7
Ibrahim Ali <hemamessi47@gmail.com>
2021-07-06 14:22:32 +0200

    add my-reports page

--- Front-end/freelance-marketplace/src/app/components/freelancer/reports/reports.module.ts
+++ Front-end/freelance-marketplace/src/app/components/freelancer/reports/reports.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { MyReportsComponent } from './my-reports/my-reports.component';
 
 const routes : Routes = [
-  {path : 'overview' , component : OverviewComponent}
+  {path : 'overview' , component : OverviewComponent},
+  {path : 'my-reports' , component : MyReportsComponent}
 ]
 
 @NgModule({
   declarations: [
-    OverviewComponent
+    OverviewComponent,
+    MyReportsComponent
   ],
   imports: [
     CommonModule,RouterModule.forChild(routes),SharedModule
