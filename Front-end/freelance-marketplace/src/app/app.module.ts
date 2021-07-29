9c204ad81e2d5dd9eb57577d1de58ee915d07dc4
amroesmail98 <amroesmail98@gmail.com>
2021-07-29 04:06:31 +0200

    add name inputs

--- Front-end/freelance-marketplace/src/app/app.module.ts
+++ Front-end/freelance-marketplace/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AccordionModule} from 'primeng/accordion';
-import { RegisterComponent } from './register/register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
+
   ],
   imports: [
     BrowserModule,
