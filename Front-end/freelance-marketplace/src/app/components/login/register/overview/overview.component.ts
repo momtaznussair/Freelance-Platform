c502e3d516887f200b7c78ba41650de9ec30af9a
Ibrahim Ali <hemamessi47@gmail.com>
2021-07-31 23:29:40 +0200

    add overview component

---
+++ Front-end/freelance-marketplace/src/app/components/login/register/overview/overview.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+
+  form : FormGroup = new FormGroup({});
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      overview : ['' , [ Validators.required, Validators.maxLength(500) , Validators.minLength(250)] ],
+      jobTitle : ['' , [Validators.required , Validators.minLength(10) , Validators.maxLength(255)]]
+    })
+  }
+
+}
