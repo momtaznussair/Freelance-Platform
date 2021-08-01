97284aed2f65ce783c8179452296d977eba20446
amira <amira95ibrahim@gmail.com>
2021-08-01 18:43:18 +0200

    updating expertise functionality

--- Front-end/freelance-marketplace/src/app/app.module.ts
+++ Front-end/freelance-marketplace/src/app/app.module.ts
@@ -5,9 +5,18 @@ import { AppComponent } from './app.component';
 import {HttpClientModule , HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AccordionModule} from 'primeng/accordion';
 import { TokenInterceptorService } from './services/token-interceptor.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatOption } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent,
 
@@ -17,6 +26,15 @@ import { TokenInterceptorService } from './services/token-interceptor.service';
     AppRoutingModule,
     HttpClientModule,
     AccordionModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatOption,
+  ReactiveFormsModule,
+  FormsModule,
+
+  
   ],
   providers: [
     {
--- Front-end/freelance-marketplace/src/app/components/login/register/Expertise/expertise/expertise.component.ts
+++ Front-end/freelance-marketplace/src/app/components/login/register/Expertise/expertise/expertise.component.ts
@@ -3,21 +3,48 @@ import {RegisterDataService} from "../../../../../services/register-data.service
 import { FormGroup } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
+export interface User {
+  name: string;
+}
 @Component({
   selector: 'app-expertise',
   templateUrl: './expertise.component.html',
   styleUrls: ['./expertise.component.css']
 })
 export class ExpertiseComponent implements OnInit {
+
+  myControl = new FormControl();
+  options: User[] = [
+    {name: 'Mary'},
+    {name: 'Shelley'},
+    {name: 'Igor'}
+  ];
+  filteredOptions: Observable<User[]> |undefined
+  ;
+
+
+  //////////////////////////////////////////////
   form : FormGroup = new FormGroup({});
   message:any;
   approvalText:string="";
   skills=["css","html","javascript","react"];
   selectedSkills=[""];
+  result=[""];
   constructor(private formBuilder : FormBuilder ,private appService:RegisterDataService) { }
 
   ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.options.slice())
+      );
+    //////////////////////////////////////////////
     
  this.appService.currentApprovalStageMessage.subscribe(msg => this.message = msg);
 
@@ -25,7 +52,18 @@ export class ExpertiseComponent implements OnInit {
       searchSkill : ['' , [ Validators.required, Validators.maxLength(500) , Validators.minLength(10)]]
     })
   }
+////////////////////////
 
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+///////////////////////////////////////////////
   submit()
  {
   console.log(this.approvalText)
@@ -35,5 +73,19 @@ export class ExpertiseComponent implements OnInit {
  addSkill(b:HTMLElement){
   this.selectedSkills.push(b.innerText);
    console.log(this.selectedSkills)
+ }
+ search(){
+   console.log(this.approvalText);
+
+
+
+ this.result = this.skills.filter(s => s.includes(this.approvalText));
+
+console.log(this.result);
+
+
+
+
+  //  this.skills.filter
  }
 }
