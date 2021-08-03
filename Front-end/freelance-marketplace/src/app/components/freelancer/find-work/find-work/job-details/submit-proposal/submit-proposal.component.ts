72d2c06bb5b4fb5cebe3e2ee02f71ade77237209
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-04 00:03:02 +0200

    edit submit proposal path

--- Front-end/freelance-marketplace/src/app/components/freelancer/find-work/find-work/job-details/submit-proposal/submit-proposal.component.ts
+++ Front-end/freelance-marketplace/src/app/components/freelancer/find-work/find-work/job-details/submit-proposal/submit-proposal.component.ts
@@ -12,22 +12,22 @@ export class SubmitProposalComponent implements OnInit {
   // proposal:Proposal[]=[];
 
     // form:FormGroup;
-  constructor(private proposalservice:ProposalService,private _formBuilder:FormBuilder) { }
+  // constructor(private proposalservice:ProposalService,private _formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
     // this.form=this._formBuilder.group({
-       
+
     // });
   }
-  submitproposal(hourrate:any,paymentamount:any,coverletter:any,attatchment:string){
-    let proposals:Proposal=new Proposal();
-    proposals.hourly_rate=hourrate;
-    proposals.payment_amount=paymentamount;
-    proposals.cover_letter=coverletter;
-    proposals.attachment=attatchment;
-    this.proposalservice.addproposal(proposals).subscribe(Response=>{
-      console.log(Response);
-    },error=>console.error);
-  }
+  // submitproposal(hourrate:any,paymentamount:any,coverletter:any,attatchment:string){
+  //   let proposals:Proposal=new Proposal();
+  //   proposals.hourly_rate=hourrate;
+  //   proposals.payment_amount=paymentamount;
+  //   proposals.cover_letter=coverletter;
+  //   proposals.attachment=attatchment;
+  //   this.proposalservice.addproposal(proposals).subscribe(Response=>{
+  //     console.log(Response);
+  //   },error=>console.error);
+  // }
 
 }
