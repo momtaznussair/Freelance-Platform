1ec852fc6382de543c5a3e280242bd9f5100263c
MoEmam203 <mostafa.emamdar4@gmail.com>
2021-08-05 09:12:27 +0200

    edit in destroy method in Language Controller

--- backend/app/Http/Controllers/Api/LanguageController.php
+++ backend/app/Http/Controllers/Api/LanguageController.php
@@ -73,6 +73,7 @@ class LanguageController extends Controller
         $language = Language::find($id);
 
         if($language){
+            $language->jobs()->delete();
             $language->delete();
             return $this->apiResponse(true,'',200);
         }
