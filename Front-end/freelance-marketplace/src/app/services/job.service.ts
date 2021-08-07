2af44eb38fdbd586f11b5a1449007bccdc1f2404
menna <fullaloka38@gmail.com>
2021-08-07 20:57:14 +0200

    mm

--- Front-end/freelance-marketplace/src/app/services/job.service.ts
+++ Front-end/freelance-marketplace/src/app/services/job.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 
@@ -6,14 +7,14 @@ import { ApiService } from './api.service';
 })
 export class JobService {
 
-  constructor(private _apiservice:ApiService) { }
+  constructor(private _apiservice:ApiService,private http:HttpClient) { }
 
   get(){
-    return this._apiservice.get("");
+    return this._apiservice.get("http://127.0.0.1:8000/api/jobs");
   
   }
   addJob(body:any){
-    return this._apiservice.post("",body);
+    return this._apiservice.post("http://127.0.0.1:8000/api/jobs",body);
   }
 
   // delete(id:number){
