3f9d1eacae427ce6bd2543b392cf825d01c3bb53
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-09 11:25:49 +0200

    resolve conflict

--- Front-end/freelance-marketplace/src/app/components/login/register/Eduction/eduction/eduction.component.ts
+++ Front-end/freelance-marketplace/src/app/components/login/register/Eduction/eduction/eduction.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
-<<<<<<< HEAD
 import { ApiService } from 'src/app/services/api.service';
 import { environment } from 'src/environments/environment.prod';
-=======
-// import {RegisterDataService} from "../../../../../services/register-data.service";
-import { UserService } from 'src/app/services/user.service';
->>>>>>> 3a63b5880f0c09adf46c7c27ec075df81e7859b3
 
 @Component({
   selector: 'app-eduction',
@@ -18,7 +13,6 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class EductionComponent implements OnInit {
 
-<<<<<<< HEAD
   user_id : any;
   form : FormGroup = new FormGroup({});
   constructor(private formBuilder : FormBuilder  , private router : Router , private apiService : ApiService) { }
@@ -31,7 +25,7 @@ export class EductionComponent implements OnInit {
     this.currentRegisterstart_date = localStorage.getItem('start_date');
 
     this.form = this.formBuilder.group({
-      user_id : ['1' , [ Validators.required]],
+      user_id : [this.user_id , [ Validators.required]],
       institute : ['' , [ Validators.required , Validators.minLength(10) , Validators.maxLength(250) ]],
       area_of_study : ['' , [Validators.required , Validators.minLength(10) , Validators.maxLength(250) ]],
       degree : ['' , [Validators.required , Validators.minLength(10) , Validators.maxLength(250) ]],
@@ -44,58 +38,13 @@ export class EductionComponent implements OnInit {
   next()
 {
 
-  console.log({user_id : this.user_id , institute : this.form.controls['institute'].value , area_of_study : this.form.controls['area_of_study'].value});
+  console.log(this.form.value);
   if(this.form.valid)
   {
     this.apiService.post(`${environment.apiUrl}/educations` , this.form.value).subscribe(response=>{
       console.log(response);
       this.router.navigateByUrl("/user/signup/lang");
     },error=>console.error);
-=======
-
-  form : FormGroup = new FormGroup({});
-  constructor(private formBuilder : FormBuilder  , private router : Router) { }
-
-  currentRegisterData : any;
-
-
-  // msg = localStorage.getItem('msg');
-  // constructor(private appService:RegisterDataService,private apiService : ApiService , private registerService : RegisterDataService) { }
-
-  // name : string = '';
-  // institute : string = '';
-  // graduationdate : any ='';
-
-  // eduction : Eduction = new Eduction();
-
-
-
-  ngOnInit(): void {
-
-    this.currentRegisterData = localStorage.getItem('data');
-
-    this.form = this.formBuilder.group({
-      inistiute : ['' , [ Validators.required ]],
-      areaofstudy : ['' , [Validators.required ]],
-      degree : ['' , [Validators.required ]],
-      data : ['' , [Validators.required ]]
-
-    })
-  }
-  isLogged : boolean = false;
-
-  next()
-{
-  if(this.form.valid)
-  {
-    this.currentRegisterData = JSON.parse(this.currentRegisterData)
-    this.currentRegisterData.inistiute = this.form.controls.inistiute.value;
-    this.currentRegisterData.areaofstudy = this.form.controls.areaofstudy.value;
-    this.currentRegisterData.degree = this.form.controls.degree.value;
-    // this.currentRegisterData.graduation_date = this.form.controls.graduation_date.value;
-    localStorage.setItem('data' ,JSON.stringify(this.currentRegisterData));
-    this.router.navigateByUrl("/user/signup/lang");
->>>>>>> 3a63b5880f0c09adf46c7c27ec075df81e7859b3
   }
   else
   {
@@ -104,14 +53,5 @@ export class EductionComponent implements OnInit {
 }
 }
 
-<<<<<<< HEAD
-=======
-    // this.currentRegisterData = localStorage.getItem('data');
-    // this.apiService.get(`${environment.apiUrl}/eduction`).subscribe(response =>{
-    //   this.eduction = response;
-    //   console.log(this.eduction);
-    // },error=>console.error);
-
->>>>>>> 3a63b5880f0c09adf46c7c27ec075df81e7859b3
 
 
