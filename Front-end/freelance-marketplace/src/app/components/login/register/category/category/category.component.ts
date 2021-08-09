af6abc6c27683de407646b65141e0892fc9adfa0
Ibrahim Ali <hemamessi47@gmail.com>
2021-08-09 23:34:52 +0200

    update category

--- Front-end/freelance-marketplace/src/app/components/login/register/category/category/category.component.ts
+++ Front-end/freelance-marketplace/src/app/components/login/register/category/category/category.component.ts
@@ -32,6 +32,16 @@ export class CategoryComponent implements OnInit {
 
   ngOnInit(): void {
 
+    //test request location
+    this.apiService.get("https://www.universal-tutorial.com/api/countries/",{ 'headers': {
+      'Accept' : 'application/json',
+      'Authorization' : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJhbWlyYTk1YmFkckBnbWFpbC5jb20iLCJhcGlfdG9rZW4iOiJvSXFDZEdkbkFzSGNVbE8zQTc4UGl4VENEelpqUHZGc0dJZ`
+    }}).subscribe(res =>{
+      console.log(res)
+    },error=>{console.log(error)});
+
+    //end of test location
+
     localStorage.removeItem('data');
 
     this.currentRegisterData = localStorage.getItem('user_data');
