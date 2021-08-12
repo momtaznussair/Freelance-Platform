2e283b0ec882b6dd5e9c46ab72cb044ce4a280f5
menna <fullaloka38@gmail.com>
2021-08-13 01:52:05 +0200

    freelancers service and ts

--- Front-end/freelance-marketplace/src/app/components/landing-page/freelancers/freelancers.component.ts
+++ Front-end/freelance-marketplace/src/app/components/landing-page/freelancers/freelancers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FreelancersService } from 'src/app/services/freelancers.service';
 
 @Component({
   selector: 'app-freelancers',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FreelancersComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private freelance:FreelancersService) { }
+freelancers:any;
   ngOnInit(): void {
+    this.freelance.getFreelancers(`freelancers`).subscribe(res=>{
+      this.freelancers=res.data;
+      console.log(this.freelancers);
+    },error=>console.error)
   }
 
 }
