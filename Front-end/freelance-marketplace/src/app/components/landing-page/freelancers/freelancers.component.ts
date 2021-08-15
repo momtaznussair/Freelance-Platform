a9d30918b5c9364533393c85a204123ffcd3b104
menna <fullaloka38@gmail.com>
2021-08-15 02:21:27 +0200

    display data in freelancers

--- Front-end/freelance-marketplace/src/app/components/landing-page/freelancers/freelancers.component.ts
+++ Front-end/freelance-marketplace/src/app/components/landing-page/freelancers/freelancers.component.ts
@@ -16,8 +16,8 @@ freelancer_id:any;
     this.freelance.getFreelancers(`freelancers`).subscribe(res=>{
       this.freelancers=res.data;
       console.log(this.freelancers);
-      this.freelancer_id=this.freelancers[1].id;
-      console.log(this.freelancer_id);
+      // this.freelancer_data=this.freelancers.i;
+      // console.log(this.freelancer_data);
 
     },error=>console.error)
   }
