6961b2c44b35c3ed9e4eb7978832c20ac2612496
amira <amira95ibrahim@gmail.com>
2021-08-26 01:08:31 +0200

    upload image

---
+++ Front-end/freelance-marketplace/src/app/services/upload-img.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadImgService {
+
+  constructor(private http:HttpClient) { }
+   // Returns an observable
+   upload(file:File):Observable<any> {
+  
+    // Create form data
+    const formData = new FormData(); 
+      
+    // Store form name as "file" with file data
+    formData.append("file", file,file.name);
+      // console.log(formData);
+
+    // Make http post request over api  with formData as req
+    return this.http.post(`${environment.apiUrl}/register`, file)
+}
+}
